82a2bb21051fe9dc07ca9b8491d41aa8fe2776db
Arman Patel <vanixgaming634@gmail.com>
2023-05-08 13:30:20 +0530

    Solved

---
+++ 05_Greatest_of_2_num.js
@@ -0,0 +1,16 @@
+// Find greatest number among 2 numbers
+
+let no1 = 5,no2 =  56;
+
+const checkNum = (num1,num2) =>{
+    if(num1>num2)
+    {
+        console.log(num1 + " is greatest");
+    }
+    else if(num1<num2)
+    {
+        console.log(num2 + " is greatest");
+
+    }
+}
+checkNum(no1,no2); 
