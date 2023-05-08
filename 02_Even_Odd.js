1d0aa17d22560d4b136bf26c77aa4262e0d701d3
Arman Patel <vanixgaming634@gmail.com>
2023-05-08 13:15:45 +0530

    Solved

---
+++ 02_Even_Odd.js
@@ -0,0 +1,15 @@
+// Check whether given number is even or odd
+
+let no = 4;
+
+const checkNum = (no) =>{
+    if(no % 2 == 0)
+    {
+        console.log("The given number is even");
+    }
+    else
+    {
+        console.log("The given number is odd");
+
+    }
+}
